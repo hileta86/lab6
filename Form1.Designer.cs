c42b8a393a415e0de40d38b27c8ed6b44a8ea972
hileta86 <ihorhileta@gmail.com>
2025-11-17 02:05:07 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A_matrix_dgv = new System.Windows.Forms.DataGridView();
+            this.C_matrix_dgv = new System.Windows.Forms.DataGridView();
+            this.B_vector_dgv = new System.Windows.Forms.DataGridView();
+            this.X_vector_dgv = new System.Windows.Forms.DataGridView();
+            this.BСreateGrid = new System.Windows.Forms.Button();
+            this.BClear = new System.Windows.Forms.Button();
+            this.BClose = new System.Windows.Forms.Button();
+            this.NUD_rozmir = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbMethod = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.A_matrix_dgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C_matrix_dgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B_vector_dgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.X_vector_dgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_rozmir)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // A_matrix_dgv
+            // 
+            this.A_matrix_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.A_matrix_dgv.ColumnHeadersVisible = false;
+            this.A_matrix_dgv.Location = new System.Drawing.Point(32, 66);
+            this.A_matrix_dgv.Name = "A_matrix_dgv";
+            this.A_matrix_dgv.RowHeadersVisible = false;
+            this.A_matrix_dgv.Size = new System.Drawing.Size(348, 150);
+            this.A_matrix_dgv.TabIndex = 0;
+            this.A_matrix_dgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.A_matrix_dgv_CellClick);
+            // 
+            // C_matrix_dgv
+            // 
+            this.C_matrix_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.C_matrix_dgv.ColumnHeadersVisible = false;
+            this.C_matrix_dgv.Location = new System.Drawing.Point(32, 249);
+            this.C_matrix_dgv.Name = "C_matrix_dgv";
+            this.C_matrix_dgv.RowHeadersVisible = false;
+            this.C_matrix_dgv.Size = new System.Drawing.Size(348, 150);
+            this.C_matrix_dgv.TabIndex = 1;
+            // 
+            // B_vector_dgv
+            // 
+            this.B_vector_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.B_vector_dgv.ColumnHeadersVisible = false;
+            this.B_vector_dgv.Location = new System.Drawing.Point(464, 66);
+            this.B_vector_dgv.Name = "B_vector_dgv";
+            this.B_vector_dgv.RowHeadersVisible = false;
+            this.B_vector_dgv.Size = new System.Drawing.Size(125, 150);
+            this.B_vector_dgv.TabIndex = 2;
+            this.B_vector_dgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.B_vector_dgv_CellClick);
+            // 
+            // X_vector_dgv
+            // 
+            this.X_vector_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.X_vector_dgv.ColumnHeadersVisible = false;
+            this.X_vector_dgv.Location = new System.Drawing.Point(626, 66);
+            this.X_vector_dgv.Name = "X_vector_dgv";
+            this.X_vector_dgv.ReadOnly = true;
+            this.X_vector_dgv.RowHeadersVisible = false;
+            this.X_vector_dgv.Size = new System.Drawing.Size(125, 150);
+            this.X_vector_dgv.TabIndex = 3;
+            // 
+            // BСreateGrid
+            // 
+            this.BСreateGrid.Location = new System.Drawing.Point(464, 249);
+            this.BСreateGrid.Name = "BСreateGrid";
+            this.BСreateGrid.Size = new System.Drawing.Size(125, 47);
+            this.BСreateGrid.TabIndex = 4;
+            this.BСreateGrid.Text = "Розв\'язати";
+            this.BСreateGrid.UseVisualStyleBackColor = true;
+            this.BСreateGrid.Click += new System.EventHandler(this.BСreateGrid_Click);
+            // 
+            // BClear
+            // 
+            this.BClear.Location = new System.Drawing.Point(626, 249);
+            this.BClear.Name = "BClear";
+            this.BClear.Size = new System.Drawing.Size(125, 47);
+            this.BClear.TabIndex = 5;
+            this.BClear.Text = "Очистити";
+            this.BClear.UseVisualStyleBackColor = true;
+            this.BClear.Click += new System.EventHandler(this.BClear_Click);
+            // 
+            // BClose
+            // 
+            this.BClose.Location = new System.Drawing.Point(464, 302);
+            this.BClose.Name = "BClose";
+            this.BClose.Size = new System.Drawing.Size(287, 48);
+            this.BClose.TabIndex = 6;
+            this.BClose.Text = "Вихід";
+            this.BClose.UseVisualStyleBackColor = true;
+            this.BClose.Click += new System.EventHandler(this.BClose_Click);
+            // 
+            // NUD_rozmir
+            // 
+            this.NUD_rozmir.Location = new System.Drawing.Point(260, 20);
+            this.NUD_rozmir.Name = "NUD_rozmir";
+            this.NUD_rozmir.Size = new System.Drawing.Size(120, 20);
+            this.NUD_rozmir.TabIndex = 7;
+            this.NUD_rozmir.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NUD_rozmir.ValueChanged += new System.EventHandler(this.NUD_rozmir_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Оберіть розмір матриці А";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Матриця А коефіцієнтів СЛАР";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Матриця С коефіцієнтів L / U розкладу";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(461, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Вектор В";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(623, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Вектор Х";
+            // 
+            // cbMethod
+            // 
+            this.cbMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMethod.FormattingEnabled = true;
+            this.cbMethod.Items.AddRange(new object[] {
+            "L/U-розклад",
+            "Метод Гауса"});
+            this.cbMethod.Location = new System.Drawing.Point(630, 19);
+            this.cbMethod.Name = "cbMethod";
+            this.cbMethod.Size = new System.Drawing.Size(121, 21);
+            this.cbMethod.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(544, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Оберіть метод";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbMethod);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NUD_rozmir);
+            this.Controls.Add(this.BClose);
+            this.Controls.Add(this.BClear);
+            this.Controls.Add(this.BСreateGrid);
+            this.Controls.Add(this.X_vector_dgv);
+            this.Controls.Add(this.B_vector_dgv);
+            this.Controls.Add(this.C_matrix_dgv);
+            this.Controls.Add(this.A_matrix_dgv);
+            this.Name = "Form1";
+            this.Text = "Г.І. Метод L/U перетворення для розв\'язання СЛАР.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.A_matrix_dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C_matrix_dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B_vector_dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.X_vector_dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_rozmir)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView A_matrix_dgv;
+        private System.Windows.Forms.DataGridView C_matrix_dgv;
+        private System.Windows.Forms.DataGridView B_vector_dgv;
+        private System.Windows.Forms.DataGridView X_vector_dgv;
+        private System.Windows.Forms.Button BСreateGrid;
+        private System.Windows.Forms.Button BClear;
+        private System.Windows.Forms.Button BClose;
+        private System.Windows.Forms.NumericUpDown NUD_rozmir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbMethod;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int N = 1; 
+        int i = 0;
+        int j = 0;
+        int Change;
+        double[,] A = new double[6, 6]; 
+        double[] B = new double[6]; 
+        double[] X = new double[6];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Decomp(int N, ref int Change)
+        {
+            int i, j, k;
+            double R, L, U;
+
+            Change = 1;
+
+            R = Math.Abs(A[1, 1]);
+            for (j = 2; j <= N; j++)
+            {
+                if (Math.Abs(A[j, 1]) >= R)
+                {
+                    Change = j;
+                    R = Math.Abs(A[j, 1]);
+                }
+            }
+
+            if (R <= 1e-7)
+            {
+                MessageBox.Show("Система вироджена");
+                return;
+            }
+
+            if (Change != 1)
+            {
+                for (i = 1; i <= N; i++)
+                {
+                    R = A[Change, i];
+                    A[Change, i] = A[1, i];
+                    A[1, i] = R;
+                }
+            }
+
+            for (i = 2; i <= N; i++)
+            {
+                A[1, i] = A[1, i] / A[1, 1];
+            }
+
+            for (i = 2; i <= N; i++)
+            {
+                for (k = i; k <= N; k++)
+                {
+                    R = 0.0;
+                    for (j = 1; j <= i - 1; j++)
+                    {
+                        R += A[k, j] * A[j, i];
+                    }
+                    A[k, i] = A[k, i] - R;
+                }
+
+                if (Math.Abs(A[i, i]) <= 1e-7)
+                {
+                    MessageBox.Show("Система вироджена");
+                    return;
+                }
+
+                for (k = i + 1; k <= N; k++)
+                {
+                    R = 0.0;
+                    for (j = 1; j <= i - 1; j++)
+                    {
+                        R += A[i, j] * A[j, k];
+                    }
+                    A[i, k] = (A[i, k] - R) / A[i, i];
+                }
+            }
+
+            for (i = 0; i < N; i++)
+                for (j = 0; j < N; j++)
+                {
+                    C_matrix_dgv.Rows[i].Cells[j].Value = Convert.ToString(A[i + 1, j + 1]);
+                }
+        }
+
+        private void Solve(int Change, int N)
+        {
+            int i, j;
+            double R;
+
+            if (Change != 1)
+            {
+                R = B[Change];
+                B[Change] = B[1];
+                B[1] = R;
+            }
+
+            B[1] = B[1] / A[1, 1];
+
+            for (i = 2; i <= N; i++)
+            {
+                R = 0.0;
+                for (j = 1; j <= i - 1; j++)
+                {
+                    R += A[i, j] * B[j];
+                }
+                B[i] = (B[i] - R) / A[i, i];
+            }
+
+            X[N] = B[N];
+
+            for (i = 1; i <= N - 1; i++)
+            {
+                R = 0.0;
+                for (j = N + 1 - i; j <= N; j++)
+                {
+                    R += A[N - i, j] * X[j];   
+                }
+                X[N - i] = B[N - i] - R;
+            }
+        }
+
+        private void Gauss(int N)
+        {
+            int i, j, k;
+            double R;
+
+            if (N == 1)
+            {
+                if (Math.Abs(A[1, 1]) < 1e-7)
+                {
+                    MessageBox.Show("Система є виродженою");
+                    return;
+                }
+                X[1] = B[1] / A[1, 1];
+                return;
+            }
+
+            for (i = 1; i <= N - 1; i++)
+            {
+                k = i;
+                R = Math.Abs(A[i, i]);
+                for (j = i + 1; j <= N; j++)
+                {
+                    if (Math.Abs(A[j, i]) >= R)
+                    {
+                        k = j;
+                        R = Math.Abs(A[j, i]);
+                    }
+                }
+
+                if (R <= 1e-7)
+                {
+                    MessageBox.Show("Система є виродженою");
+                    return;
+                }
+
+                if (k != i)
+                {
+                    R = B[k];
+                    B[k] = B[i];
+                    B[i] = R;
+
+                    for (j = i; j <= N; j++)
+                    {
+                        R = A[k, j];
+                        A[k, j] = A[i, j];
+                        A[i, j] = R;
+                    }
+                }
+
+                R = A[i, i];
+                B[i] = B[i] / R;
+                for (j = i; j <= N; j++)
+                {
+                    A[i, j] = A[i, j] / R;
+                }
+
+                for (k = i + 1; k <= N; k++)
+                {
+                    R = A[k, i];
+                    B[k] = B[k] - R * B[i];
+                    A[k, i] = 0.0;
+                    for (j = i + 1; j <= N; j++)
+                    {
+                        A[k, j] = A[k, j] - R * A[i, j];
+                    }
+                }
+            }
+
+            if (Math.Abs(A[N, N]) <= 1e-7)
+            {
+                MessageBox.Show("Система є виродженою");
+                return;
+            }
+
+            X[N] = B[N] / A[N, N];
+
+            for (i = N - 1; i >= 1; i--)
+            {
+                R = B[i];
+                for (j = i + 1; j <= N; j++)
+                {
+                    R = R - A[i, j] * X[j];
+                }
+                X[i] = R;
+            }
+
+            for (i = 0; i < N; i++)
+                for (j = 0; j < N; j++)
+                    C_matrix_dgv.Rows[i].Cells[j].Value = A[i + 1, j + 1];
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            X_vector_dgv.ReadOnly = true; 
+            A_matrix_dgv.AllowUserToAddRows = false;
+            B_vector_dgv.AllowUserToAddRows = false;
+            X_vector_dgv.AllowUserToAddRows = false;
+            A_matrix_dgv.ColumnCount = 1;
+            A_matrix_dgv.RowCount = 1;
+            X_vector_dgv.ColumnCount = 1;
+            X_vector_dgv.RowCount = 1;
+            B_vector_dgv.ColumnCount = 1;
+            B_vector_dgv.RowCount = 1;
+        }
+
+        private void NUD_rozmir_ValueChanged(object sender, EventArgs e)
+        {
+            N = Convert.ToInt16(NUD_rozmir.Value);
+            A_matrix_dgv.RowCount = N;
+            A_matrix_dgv.ColumnCount = N;
+            X_vector_dgv.RowCount = N;
+            B_vector_dgv.RowCount = N;
+            C_matrix_dgv.RowCount = N;
+            C_matrix_dgv.ColumnCount = N;
+        }
+
+        private void BСreateGrid_Click(object sender, EventArgs e)
+        {
+            bool exc_A = false;
+            bool exc_B = false;
+
+            for (i = 1; i <= N; i++)
+                for (j = 1; j <= N; j++)
+                {
+                    try
+                    {
+                        A[i, j] = Convert.ToDouble(A_matrix_dgv[j - 1, i - 1].Value);
+                    }
+                    catch
+                    {
+                        A_matrix_dgv[j - 1, i - 1].Style.ForeColor = Color.Red;
+                        exc_A = true;
+                    }
+                }
+
+            for (j = 0; j < N; j++)
+            {
+                try
+                {
+                    B[j + 1] = Convert.ToDouble(B_vector_dgv[0, j].Value);
+                }
+                catch
+                {
+                    B_vector_dgv[0, j].Style.ForeColor = Color.Red;
+                    exc_B = true;
+                }
+            }
+
+            if (exc_A || exc_B)
+            {
+                MessageBox.Show("Помилка введення!");
+                return;
+            }
+
+            if (cbMethod.SelectedIndex == 0)     
+            {
+                Decomp(N, ref Change);
+                Solve(Change, N);
+            }
+            else                                 
+            {
+                Gauss(N);
+            }
+
+            {
+                for (i = 0; i < N; i++)
+                    X_vector_dgv[0, i].Value = X[i + 1].ToString();
+
+                MessageBox.Show("Розв'язок знайдено");
+            }
+        }
+
+        private void BClear_Click(object sender, EventArgs e)
+        {
+            for (i = 0; i < N; i++)
+                for (j = 0; j < N; j++)
+                {
+                    A_matrix_dgv[j, i].Value = "";
+                    C_matrix_dgv[j, i].Value = "";
+                }
+            for (j = 0; j < N; j++)
+            {
+                B_vector_dgv[0, j].Value = "";
+                X_vector_dgv[0, j].Value = "";
+            }
+        }
+
+        private void BClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void A_matrix_dgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            A_matrix_dgv.CurrentCell.Style.ForeColor = Color.Black;
+        }
+
+        private void B_vector_dgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            B_vector_dgv.CurrentCell.Style.ForeColor = Color.Black;
+        }
+    }
+}
